b7cc3a78492e844ba2454ba2ab3c8bf8f53035cb
Jason Narrainen <jason.narrainen@protonmail.com>
2023-05-12 13:32:50 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+
+import {
+  Routes,
+  Route
+} from 'react-router-dom'
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+    </Routes>
+  )
+}
+
+export default App
